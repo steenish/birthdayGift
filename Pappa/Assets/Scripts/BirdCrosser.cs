dcf95a057cb40289258ce12eb3175b92453b12c5
Anders Steen <anders.steen98@gmail.com>
2020-08-08 12:07:12 +0200

    add birds

---
+++ Pappa/Assets/Scripts/BirdCrosser.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdCrosser : MonoBehaviour {
+
+	[SerializeField]
+	private Transform playerTransform;
+	[SerializeField]
+	private Transform[] crosses;
+
+    void Update() {
+        foreach (Transform cross in crosses) {
+			if (playerTransform.position.x > cross.position.x) cross.gameObject.SetActive(true);
+			else cross.gameObject.SetActive(false);
+		}
+    }
+}
