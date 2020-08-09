bfcf2e05a4cad69b73a39c8fefa075ba3d269498
Anders Steen <anders.steen98@gmail.com>
2020-08-09 12:58:46 +0200

    add text

--- Pappa/Assets/Scripts/BirdCrosser.cs
+++ Pappa/Assets/Scripts/BirdCrosser.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class BirdCrosser : MonoBehaviour {
 
+#pragma warning disable
 	[SerializeField]
 	private Transform playerTransform;
 	[SerializeField]
 	private Transform[] crosses;
+#pragma warning restore
 
-    void Update() {
+	void Update() {
         foreach (Transform cross in crosses) {
 			if (playerTransform.position.x > cross.position.x) cross.gameObject.SetActive(true);
 			else cross.gameObject.SetActive(false);
---
+++ Pappa/Assets/Scripts/TextDisplayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDisplayer : MonoBehaviour {
+
+#pragma warning disable
+	[SerializeField]
+	private SpriteRenderer renderer;
+	[SerializeField]
+	private Transform playerTransform;
+#pragma warning restore
+
+	void Update() {
+		if (playerTransform.position.x + 0.4f >= transform.position.x) renderer.enabled = true;
+    }
+}
